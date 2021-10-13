db56a251fc5ae2b0ecd78b108261d705924ac62c
USER <USER@MyMSI>
2021-10-13 16:14:22 +0700

    commit

---
+++ src/main/java/Ham/b1.java
@@ -0,0 +1,9 @@
+package Ham;
+public class b1 {
+    public static void main (String[] args){
+        Tb();
+    }
+    public static void Tb(){
+    System.out.println("Lap trinh Java 2021");
+}
+}
