74a867a938c42e749f889b0acc84e3456916497d
USER <>
2021-10-13 16:14:42 +0700

    commit

---
+++ src/main/java/Ham/b2.java
@@ -0,0 +1,12 @@
+package Ham;
+public class b2 {
+    public static void main (String[] args){
+        string();
+    }
+    public static void string(){
+        String n;
+        n="Title";
+        System.out.println(n);
+    }
+    
+}
